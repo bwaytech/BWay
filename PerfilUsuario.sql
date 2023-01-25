cdfe3e5c775e33bae27fcefd79981b1223e8f7b0
Bruno Petterson Elis de Oliveira <bruno.pedoliveira@hsl.org.br>
2023-01-25 11:34:26 -0300

    Criação scripts de insert nas tabelas domínio

---
+++ PerfilUsuario.sql
@@ -0,0 +1,32 @@
+INSERT INTO PERFIL_USUARIO(
+	TIPO
+,	DESCRICAO
+,	SITUACAO
+)
+VALUES(
+	'Corretor'
+,	'Acesso a plantões, operações e avaliações'
+,	1
+)
+
+INSERT INTO PERFIL_USUARIO(
+	TIPO
+,	DESCRICAO
+,	SITUACAO
+)
+VALUES(
+	'Gerente'
+,	'Criaçao e acesso a plantões, operações, avaliações, relatórios e movimentações'
+,	1
+)
+
+INSERT INTO PERFIL_USUARIO(
+	TIPO
+,	DESCRICAO
+,	SITUACAO
+)
+VALUES(
+	'Recepcionista'
+,	'Acesso a plantões, operações e movimentações'
+,	1
+)
